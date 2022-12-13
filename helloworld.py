6eb7836f4eab53dceded4a5a1886da41d05e78aa
greeshmagit <106822996+greeshmagit@users.noreply.github.com>
2022-12-13 18:05:42 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-Print("Welcome to python testing web page")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run('13.232.89.101', 8081)
+
