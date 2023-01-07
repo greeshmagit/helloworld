2370573438ba23d21e65fcd502212cb71c318e03
greeshmagit <106822996+greeshmagit@users.noreply.github.com>
2023-01-07 18:17:41 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,4 @@ helloworld = Flask(_name_)
 def run():
   return "Hello World"
 if _name_ == "_main_":
-  helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
+  helloworld.run(host="0.0.0.0", port=int("8081"), debug=True)
